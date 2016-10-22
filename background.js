9b74e087c2a2b6ae6764440e3475a9db51ac9619
Nicolás Santángelo <santangelonicolas@gmail.com>
2016-10-22 10:43:50 -0300

    Click notifications. Avoid spam by grouping user notifications

--- background.js
+++ background.js
@@ -1,12 +1,36 @@
 chrome.extension.onConnect.addListener(function(port) {
-  port.onMessage.addListener(function(notification) {
+  var tabs = {
+    // notificationId: tabId
+  }
 
-    chrome.notifications.create({
-      type   : 'basic',
-      title  : notification.title,
-      message: notification.text,
-      iconUrl: notification.avatar
-    })
+  chrome.notifications.onClicked.addListener(function(notificationId) {
+    var tabId = tabs[notificationId]
 
+    if (tabId) {
+      chrome.tabs.update(tabId, {
+        selected   : true,
+        active     : true
+      }, function(tab) {
+        chrome.windows.update(tab.windowId, { focused: true }, function() {
+          chrome.notifications.clear(notificationId)
+          delete tabs[notificationId]
+        })
+      })
+    }
   })
+
+  port.onMessage.addListener(function(data, port) {
+    var tab = port.sender.tab
+    var notificationId = tab.id + '-' + data.title
+
+    chrome.notifications.create(notificationId, {
+      type   : 'basic',
+      title  : data.title,
+      message: data.text,
+      iconUrl: data.avatar
+    }, function(notificationId) {
+      tabs[notificationId] = tab.id
+    })
+  })
+
 })
