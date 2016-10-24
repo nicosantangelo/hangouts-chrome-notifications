3186a5627697c4642f409372b361eb139bd022c8
Nicolás Santángelo <santangelonicolas@gmail.com>
2016-10-23 22:45:36 -0300

    Add configurable options

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
+/* Globals: configuration */
+
+//
+// Messages from the front-end
+//
+
 chrome.extension.onConnect.addListener(function(port) {
   var tabs = {
     // notificationId: tabId
@@ -21,20 +27,45 @@ chrome.extension.onConnect.addListener(function(port) {
 
   port.onMessage.addListener(function(data, port) {
     var tab = port.sender.tab
-    var notificationId = data.SID + '-' + data.name
+    var changes = data.changes
 
-    var text = data.changes.becameOnline ? 'Is now online' : data.text
-    var url = tab.url.replace(/https?:\/\//, '').split('/')[0] // https://mail.google.com/mail/u/0/#inbox => mail.google.com
+    configuration.get(function(config) {
+      if (changes.becameOnline && ! config.showOnlineNotifications) return
+      if (changes.becameUnread && ! config.showUnreadNotifications) return
 
-    chrome.notifications.create(notificationId, {
-      type   : 'basic',
-      title  : data.name,
-      iconUrl: data.avatar,
-      message: text,
-      contextMessage: 'From ' + url,
-    }, function(notificationId) {
-      tabs[notificationId] = tab.id
+      var notificationId = data.SID + '-' + data.name
+      var text = changes.becameOnline ? 'Is now online' : data.text
+      var url = tab.url.replace(/https?:\/\//, '').split('/')[0] // https://mail.google.com/mail/u/0/#inbox => mail.google.com
+
+      chrome.notifications.create(notificationId, {
+        type   : 'basic',
+        title  : data.name,
+        iconUrl: data.avatar,
+        message: text,
+        contextMessage: 'From ' + url,
+      }, function(notificationId) {
+        tabs[notificationId] = tab.id
+      })
     })
   })
+})
 
+
+//
+// On installed/updated
+//
+
+chrome.runtime.onInstalled.addListener(function(details) {
+  if (details.reason !== 'update') return
+
+  var options = {}
+
+  if(details.reason === 'update') {
+    options['justUpdated'] = 2
+  }
+
+  chrome.storage.local.set(options, function () {
+    var OPTIONS_URL = chrome.extension.getURL('options/options.html')
+    chrome.tabs.create({ url: OPTIONS_URL })
+  })
 })
