2975624f663c9f53e6349251a694c3903610e1b3
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-04-08 14:04:58 -0300

    Prettier options with support for tooltips, pave the way for disabling domains

--- options/options.js
+++ options/options.js
@@ -1,51 +1,122 @@
 /* Globals: configuration */
 
 ;(function() {
-  var saveTimeout
-  var notice = document.getElementById('notice')
-  var expirationTime = document.getElementById('expiration-time')
-  var expirationTimeValue = function() {
-    var value = parseInt(expirationTime.value)
-    return value && value > 0 ? value : null
+
+  // -----------------------------------------------------------------------------
+  // Models
+
+  var expirationTime = {
+    parseValue: function(value) {
+      value = parseInt(value, 10)
+
+      if (value && value > 0) {
+        return value
+      }
+    }
   }
-  var elements = {}
 
+  var disabledDomains = {
+    toArray: function(domains) {
+      var array = []
+
+      if (domains) {
+        domains = domains.split('\n')
+
+        for(var i = 0; i < domains.length; i++) {
+          if (array.indexOf(domains[i]) === -1) array.push(domains[i])
+        }
+      }
+
+      return array
+    },
+
+    fromArray: function(domains) {
+      return domains  ? domains.join('\n') : ''
+    }
+  }
+
+  var options = {
+    htmlElements: {},
+
+    setCurrentSaved: function() {
+      configuration.forEachCurrent(function(key, value) {
+        var element = options.htmlElements[key]
+
+        if(element.type === 'checkbox') {
+          element.checked = value
+        } else {
+          if(key === 'disabledDomains') {
+            element.value = disabledDomains.fromArray(value)
+          } else {
+            element.value = value
+          }
+        }
+      })
+    },
+
+    getFromHTML: function() {
+      var newValues = {}
+
+      configuration.forEachDefault(function(key, value) {
+        var element = options.htmlElements[key]
+        if (! element) return
+
+        if(element.type === 'checkbox') {
+          newValues[key] = element.checked
+        } else {
+          if(key === 'disabledDomains') {
+            newValues[key] = disabledDomains.toArray(element.value)
+          } else if(key === 'expirationTime') {
+            newValues[key] = expirationTime.parseValue(element.value)
+          } else {
+            newValues[key] = element.value
+          }
+        }
+      })
+
+      return newValues
+    }
+  }
   configuration.forEachDefault(function (key, value) {
-    elements[key] = document.getElementById(key)
+    options.htmlElements[key] = document.getElementById(key) // setup the htmlElements property
   })
 
-  configuration.get('expirationTime', function(time) {
-    expirationTime.value = time || ''
-  })
 
   // -----------------------------------------------------------------------------
   // Events
 
-  document.getElementById('save').addEventListener('click', function save() {
-    var newValues = { }
+  var saveTimeout = null
 
-    configuration.forEachDefault(function (key, value) {
-      if(elements[key]) {
-        newValues[key] = elements[key].checked
-      }
-    })
-
-    expirationTime.value = newValues['expirationTime'] = expirationTimeValue()
+  addEventListener('#js-save', 'click', function save() {
+    var notice = document.getElementById('notice')
+    var newValues = options.getFromHTML()
 
     configuration.set(newValues, function () {
       notice.classList.remove('hidden')
       window.scrollTo(0, document.body.scrollHeight)
       clearTimeout(saveTimeout)
-      saveTimeout = setTimeout(function () { notice.classList.add('hidden') }, 4000)
+      saveTimeout = setTimeout(function() { notice.classList.add('hidden') }, 4000)
     })
-  }, false)
+
+    options.setCurrentSaved()
+  })
 
 
-  var closeButtons = document.getElementsByClassName('close-notice')
-  Array.prototype.forEach.call(closeButtons, function(closeButton) {
-    closeButton.addEventListener('click', function() {
-      closeButton.parentElement.classList.add('hidden')
-    }, false)
+  addEventListener('.js-disable-example', 'click', function() {
+    var domainsTextarea = document.getElementById('disabledDomains')
+    var currentDomains = disabledDomains.toArray(domainsTextarea.value)
+    var href = this.dataset.href
+
+    if (currentDomains.length === 0) {
+      domainsTextarea.value = href
+    } else if (currentDomains.indexOf(href) === -1) {
+      domainsTextarea.value += '\n' + href
+    }
+  })
+
+
+  addEventListener('#js-close-notice', 'click', function() {
+    this.parentElement.classList.add('hidden')
   })
 
 
@@ -66,9 +137,17 @@
     }
   })
 
-  configuration.forEachCurrent(function(key, value) {
-    if(elements[key]) {
-      elements[key].checked = value
-    }
-  })
+  options.setCurrentSaved()
+
+
+  // -----------------------------------------------------------------------------
+  // Utils
+
+  function addEventListener(selector, event, callback) {
+    var elements = document.querySelectorAll(selector)
+
+    Array.prototype.forEach.call(elements, function(element) {
+      element.addEventListener(event, callback, false)
+    })
+  }
 })()
